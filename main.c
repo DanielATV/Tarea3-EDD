6cfcfbace633681c2f9c0eebca165e7b1ca6ef95
fabianji <fabian.jimenez@sansano.usm.cl>
2017-06-16 15:52:28 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+typedef struct Objeto{
+ int peso;
+ int valor;
+ float division;
+} Objeto;
 
 int main(){
-	FILE *fp;
+	
 	int num_obj, peso_max, i;
 
-	fp = fopen("input-edd-t3.txt","r");
-
-	fscanf(fp,"%d",&num_obj);
-
-	int *precios=(int*)malloc(sizeof(int)*num_obj);
-	int *pesos=(int*)malloc(sizeof(int)*num_obj);
 	
-	if (precios==NULL || pesos==NULL)	printf("error en manejo de memoria precios o pesos");
+	printf("Ingrese numero de objetos: ");
+	scanf("%d",&num_obj);
 
-	for(i=0; i<num_obj; i++)	fscanf(fp,"%d",&precios[i]);
 	
-	for(i=0; i<num_obj; i++)	fscanf(fp,"%d",&pesos[i]);
+	Objeto *objetos=(Objeto*)malloc(sizeof(Objeto)*num_obj);
+
+	for(i=0; i<num_obj; i++){
+		printf("Ingrese el peso %d: ",i);
+		int peso_aux;
+		scanf("%d",&peso_aux);
+		objetos[i].peso = peso_aux;
 		
-	fscanf(fp,"%d",&peso_max);
+	}
+	for(i=0; i<num_obj; i++){
+		printf("Ingrese el valor %d: ",i);
+		int valor_aux;
+		scanf("%d",&valor_aux);
+		objetos[i].valor= valor_aux;
+		objetos[i].division =(float)(objetos[i].valor)/objetos[i].peso;
+		
+	}
+	printf("Ingrese peso maximo: ");
+	scanf("%d",&peso_max);
+	
 //-------------------
 
-
-
-
-
-
-
+	//Para imprimir datos iniciales
+	/*
+	for(i=0;i<num_obj;i++){
+		printf("el objeto %d tiene peso %d valor %d y division(valor/peso) %f\n",i,objetos[i].peso,objetos[i].valor,objetos[i].division);
+	}
+	printf("cap_max = %d\n",peso_max);
+	
+	*/
 
 
 //-------------------
-	fclose(fp);
-	free(precios);
-	free(pesos);
+	
+	free(objetos);
+	
 	return 0;
 
 
