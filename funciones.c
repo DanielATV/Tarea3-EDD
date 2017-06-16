0564d3613ef42af4766f4e131824f39ac8dcddf6
DanielATV <daniel.torov@sansano.usm.cl>
2017-06-16 17:29:42 -0400

    Update funciones.c

--- funciones.c
+++ funciones.c
@@ -2,22 +2,26 @@
 void clearColaP(tColaP * p){
   p -> maxsize = 0;
   p -> largo = 0;
-  free((void *)c -> arreglo);
+  free((void *)p -> arreglo);
 }
 
 int sizeColaP(tColaP * p){
-  int i;
-  i = c ->largo;
+  int i,l;
+  i = p ->largo;
   return i;
 }
 
 void insertColaP(tColaP *p,elemento a){
   if (sizeColaP(p) == 0){
     p-> largo ++;
-    *(p-> arreglo + sizeColaP(p) = a;
+    *(p-> arreglo + sizeColaP(p)) = a;
   }
   else{
-    
+    p-> largo ++;
+    *(p-> arreglo + sizeColaP(p)) = a;
+    l = sizeColaP(p);
+    while(a->proporcion > *(p-> arreglo + l%2)){
+    }
   }
   
 }
