489d1fae3909ea3e58653440a95fb0415ed21d80
fabianji <fabian.jimenez@sansano.usm.cl>
2017-06-16 15:59:41 -0400

    Update heap.h

--- heap.h
+++ heap.h
@@ -17,15 +17,16 @@ typedef struct{
 } tColaP;
 
 // inicializa un heap como vacío
-tColaP *initColaP (int t)
+tColaP *initColaP (int t);
 // elimina todos los elementos de una cola de prioridad, dejándola vacía
 void clearColaP (tColaP *P);
 // encuentra el máximo elemento del conjunto
 elemento findMax (tColaP *P);
 // cantidad de elementos en la cola de prioridad
-int sizeColaP (tColaP *P)
+int sizeColaP (tColaP *P);
 // elimina el máximo elemento del conjunto
 void removeMax (tColaP *P);
 // inserta un elemento “item” en la cola de prioridad
-void insertColaP (tCola *P,elemento item);
+void insertColaP (tColaP *P,elemento item);
 #endif
+
