c134db4e27dc9cf028c64369e25f151347f2e702
fabianji <fabian.jimenez@sansano.usm.cl>
2017-06-17 17:39:28 -0400

    Update heap.c

--- heap.c
+++ heap.c
@@ -7,7 +7,7 @@ tColaP *initColaP(int t){
 	tColaP *P = (tColaP *)malloc(sizeof(tColaP));
 	tarreglo = (elemento *)malloc(sizeof(elemento)*t);
 	P->arreglo = tarreglo;
-	P->maxsize = t;
+	P->maxsize = t+1;
 	P->largo = 0;
 	return P;
 }
@@ -24,33 +24,86 @@ elemento findMax(tColaP *P){
 }
 
 int sizeColaP(tColaP * p){
-  int i,l;
+  int i;
   i = p ->largo;
   return i;
 }
 
 void removeMax(tColaP *P){
-	int i = 1;
+	int i;
 	elemento aux;
+
+	i = 1;
 	P->arreglo[1] = P->arreglo[P->largo];
 	P->largo --;
-	while(2*i < P->largo || 2*i+1 < P->largo;){
+
+	while(2*i < P->largo || 2*i+1 < P->largo){
 		if(P->arreglo[i].proporcion <= P->arreglo[2*i].proporcion || P->arreglo[i].proporcion <= P->arreglo[2*i+1].proporcion){
 			if (P->arreglo[2*i].proporcion <= P->arreglo[2*i+1].proporcion){
-				aux = P->arreglo[1];
+				aux = P->arreglo[i];
 				P->arreglo[i] = P->arreglo[2*i+1];
-				P->arreglo[2*i+1] = P->arreglo[i];
+				P->arreglo[2*i+1] = aux;
 				i = 2*i+1;
 			}
 			else{
-				aux = P->arreglo[1];
+				aux = P->arreglo[i];
 				P->arreglo[i] = P->arreglo[2*i];
-				P->arreglo[2*i] = P->arreglo[i];
+				P->arreglo[2*i] = aux;
 				i = 2*i;
 			}
 		}
-		else{
-			break;
-		}
+		else break;
 	}
+}
+
+
+void insertColaP(tColaP *P,elemento item)
+{
+	int i, l;
+	elemento aux;
+
+	l=P->largo;
+	item.fraccion = 0.0;
+
+	if(l == P->maxsize)
+	{
+		printf("No se puede agregar al sobrepasar tamanio maximo del heap\n");
+	}
+	else
+	{
+		P->largo++;		
+		l++;
+		P->arreglo[l]=item;
+
+		for (i=l;i/2 > 0;i=i/2)
+		{
+			if(P->arreglo[i].proporcion >= P->arreglo[i/2].proporcion)
+			{
+				aux=P->arreglo[i/2];
+				
+				P->arreglo[i/2] = P->arreglo[i];
+				P->arreglo[i] = aux;
+	
+			}
+			else break;
+		}
+	
+	}
+}
+void imprimirarbol(tColaP *P)
+{
+	printf("Arbol:\n");
+	int t = 1;
+	//printf("(P:%d,V:%d,p:%f,f:%f)\n",P->arreglo[0].peso,P->arreglo[0].valor,P->arreglo[0].proporcion,P->arreglo[0].fraccion);
+	int aux = 1;
+	while(t<=(P->largo))
+	{
+		while(t<=aux){
+		printf("(P:%d,V:%d,p:%f,f:%f)",P->arreglo[t].peso,P->arreglo[t].valor,P->arreglo[t].proporcion,P->arreglo[t].fraccion);
+		t++;
+		}
+		printf("\n");
+		aux=aux*2;
+	}
+
 }
